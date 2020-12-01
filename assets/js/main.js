0d976c453b16497c723fb5f4d8e7b8e3cd736434
Antonio <antonio.lovotrico@icloud.com>
2020-12-01 18:35:37 +0100

    Quest Completed,add loop for to check if every number insert by the user is includes inside the rndArray

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 //Handler method
 $(function(){
 
-    //creare array vuoto da riempire con 5 numeri casuali
+    //array vuoto da riempire con 5 numeri casuali creati dal pc
     var rndArray = [];
 
+    //array vuoto da riempire con i 5 numeri inseriti dall'utente
+    var numInseriti = [];
+
+    //array vuoto da riempire con i numeri indovinati dall'utente
     var numIndovinati = [];
 
     //ciclo while fin quando nell'array non saranno presenti 5 numeri differenti
@@ -25,22 +29,26 @@ $(function(){
      
     // ad un secondo di distanza dalla scomparsa dell'elenco di numeri chiedere singolarmente per ogni prompt ognuno dei cinque numeri da ricordare
     setTimeout(function() {
+
         var num1 =  Number(prompt("inserisci il primo dei cinque numeri che ricordi"));
         var num2 =  Number(prompt("inserisci il secondo dei cinque numeri che ricordi"));
         var num3 =  Number(prompt("inserisci il terzo dei cinque numeri che ricordi"));
         var num4 =  Number(prompt("inserisci il quarto dei cinque numeri che ricordi"));
         var num5 =  Number(prompt("inserisci il quinto dei cinque numeri che ricordi"));
-        if (rndArray.includes(num1)) {
-            numIndovinati.push(num1);
-            alert("hai indovinato" + num1 + "ed in totale" + numIndovinati.length + "numeri");
-        } 
-    },4000); 
 
-    
-    
-    
-       
+        //inserire i numeri scelti dall'utente nell'array preposto
+        numInseriti.push(num1 , num2 , num3 , num4 , num5);
+        console.log(numInseriti);
+        
+        //creare ciclo for che confronta i numeri scelti dall'utente con quelli random per un numero volte pari alla lunghezza dell'array
+        for (var i = 0; i < numInseriti.length; i++) {
+            if (rndArray.includes(numInseriti[i])) {
+                numIndovinati.push(numInseriti[i]);
+                console.log(numIndovinati);
+            } 
+        }
+        alert("hai indovinato" +""+ numIndovinati +""+ "per un totale di"+""+ numIndovinati.length +""+ "numeri");
+        
+    },4000);      
 
 });
-
-// || rndArray.includes(num2) || rndArray.includes(num3) || rndArray.includes(num4) || rndArray.includes(num5))
